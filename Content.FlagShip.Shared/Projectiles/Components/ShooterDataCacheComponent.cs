81d53dbb095751075e7848e0718c585ceec5e033
ulramite <ulramite@gmail.com>
2026-07-13 00:32:54 +1000

    Added networking properties to ShooterDataCache, removed ViewVariables. Unrelated to test fails.

--- Content.FlagShip.Shared/Projectiles/Components/ShooterDataCacheComponent.cs
+++ Content.FlagShip.Shared/Projectiles/Components/ShooterDataCacheComponent.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Robust.Shared.GameStates;
 
 namespace Content.FlagShip.Shared.Projectiles.Components;
 
@@ -8,13 +6,13 @@ namespace Content.FlagShip.Shared.Projectiles.Components;
 /// A class for storing data about a projectile's shooter at the time of shooting, in the instance that the shooter is deleted as the projectile is shot.
 /// Currently intended to assist <see cref="Damage.Components.RequireShooterNotOnSameGridComponent"/> in dealing with projectile grenades and hitscans.
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class ShooterDataCacheComponent : Component
 {
     /// <summary>
     /// Grid Uid the shooter was on when firing.
     /// </summary>
-    [ViewVariables(VVAccess.ReadOnly)]
+    [AutoNetworkedField]
     public EntityUid? ShooterGridUid;
 }
 
